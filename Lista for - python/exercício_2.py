59fadb056bf25697a151f5291f84c8ff5d1dd7bf
EvS444 <emanuelsacoman@gmail.com>
2022-07-29 12:34:19 -0300

    Outros códigos - FOR

---
+++ Lista for - python/exercício_2.py
@@ -0,0 +1,8 @@
+def dois():
+    i = 1
+    for i in range(i,15,1):
+        if i != 11:
+            print("{}".format(i))
+        
+if __name__ == "__main__":
+    dois()
---
+++ Lista for - python/exercício_5.py
@@ -0,0 +1,10 @@
+def cinco():
+    k=0
+    for k in range (k,20):
+        a=int(input("Informe o salário: "))
+        a=a*1.1
+        a
+        print("Salário reajustado em 10%. Novo salário: {}".format(a))
+        k=k+1
+if __name__ == "__main__":
+    cinco()
